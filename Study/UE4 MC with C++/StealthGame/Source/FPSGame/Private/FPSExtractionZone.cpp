9495ea80d39ad1988f1e56d6e0fc48145c468c93
GameForPeople <koreagamemaker@gmail.com>
2018-07-07 02:35:12 +0900

    [CPP] Adjust "Sound Component & Play Sound"
    [CPP] Adjust "Sound Component & Play Sound"

--- Study/UE4 MC with C++/StealthGame/Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Study/UE4 MC with C++/StealthGame/Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -6,6 +6,7 @@
 #include "Components/DecalComponent.h"
 #include "FPSCharacter.h"
 #include "FPSGameMode.h"
+#include "Kismet/GamePlayStatics.h"
 
 // Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
@@ -32,15 +33,26 @@ AFPSExtractionZone::AFPSExtractionZone()
 void  AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, 
 	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	UE_LOG(LogTemp, Log, TEXT("Overlapped with Extraction Zone!"));
-
 	AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);
-	if (MyPawn && MyPawn->bIsCarryingObjective) 
+	
+	if (MyPawn == nullptr)
 	{
+		return;
+	}
+
+	if (MyPawn->bIsCarryingObjective) 
+	{
+		UE_LOG(LogTemp, Log, TEXT("Overlapped with Extraction Zone!"));
+
+
 		AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
 		if (GM)
 		{
 			GM->CompleteMission(MyPawn);
 		}
+	}
+	else
+	{
+		UGameplayStatics::PlaySound2D(this, ObjectiveMissingSound);
 	}
 }
--- Study/UE4 MC with C++/StealthGame/Source/FPSGame/Public/FPSExtractionZone.h
+++ Study/UE4 MC with C++/StealthGame/Source/FPSGame/Public/FPSExtractionZone.h
@@ -30,8 +30,10 @@ protected:
 	UFUNCTION()
 	void HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UPROPERTY(EditDefaultsOnly, category = "Sounds")
+	USoundBase* ObjectiveMissingSound;
+
 public:	
 
-	
 	
 };
