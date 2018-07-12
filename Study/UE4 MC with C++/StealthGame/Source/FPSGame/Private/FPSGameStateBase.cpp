23450ced27b533bf105c64f1386e83288c0a07e4
GameForPeople <koreagamemaker@gmail.com>
2018-07-12 15:31:21 +0900

    [CPP] Adjust "PlayerController Class"
    [CPP] Adjust "PlayerController Class"

--- Study/UE4 MC with C++/StealthGame/Source/FPSGame/Private/FPSGameStateBase.cpp
+++ Study/UE4 MC with C++/StealthGame/Source/FPSGame/Private/FPSGameStateBase.cpp
@@ -2,18 +2,35 @@
 
 #include "FPSGameStateBase.h"
 
+#include "FPSPlayerController.h"
 #include "Engine/World.h"
 
 
 void AFPSGameStateBase::MulticastOnMissionComplete_Implementation(APawn* InstigatorPawn, bool bMissionSuccess) 
 {
-	for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; It++)
+//	for (FConstPawnIterator It = GetWorld()->GetPawnIterator(); It; It++)
+//	{
+//		// Please Think, Your Game have Many Pawn! OMG! OMG!
+//		APawn* Pawn = It->Get();
+//		if (Pawn && Pawn->IsLocallyControlled())
+//		{
+//			Pawn->DisableInput(nullptr);
+//		}
+//	}
+
+	for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; It++)
 	{
-		// Please Think, Your Game have Many Pawn! OMG! OMG!
-		APawn* Pawn = It->Get();
-		if (Pawn && Pawn->IsLocallyControlled())
+		AFPSPlayerController* PC = Cast<AFPSPlayerController>(It->Get());
+		if (PC && PC->IsLocalController())
 		{
-			Pawn->DisableInput(nullptr);
+			PC->OnMissionCompleted(InstigatorPawn, bMissionSuccess);
+
+			APawn* MyPawn = PC->GetPawn();
+
+			if (MyPawn)
+			{
+				MyPawn->DisableInput(PC);
+			}
 		}
 	}
 }
